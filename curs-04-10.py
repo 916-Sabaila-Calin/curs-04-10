4fa65e94c4d2f7172583224183cde45356f056bc
916-Sabaila-Calin <calin.sabaila@stud.ubbcluj.ro>
2023-10-04 22:51:07 -0700

    commit

--- curs-04-10.py
+++ curs-04-10.py
@@ -1,1 +1,13 @@
-print("Hello world")
+while True:
+    print("1. Enter numbers")
+    print("2. Exit")
+    option = input(">")
+
+    if option == "1":
+        numbers = input("enter numbers: ")
+        print(numbers)
+    elif option == "2":
+        print("bye")
+        break
+    else:
+        print("error")
