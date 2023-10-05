9833bce8b0299cc3f592b31863214c6994abf7b5
916-Sabaila-Calin <calin.sabaila@stud.ubbcluj.ro>
2023-10-04 23:26:32 -0700

    2nd commit

--- curs-04-10.py
+++ curs-04-10.py
@@ -1,3 +1,14 @@
+def is_prime(n :int) -> bool:
+    if n <= 3:
+        return n >= 2
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    d = 5
+    while d * d <= n:
+        if n % d == 0 or n % (d + 2) == 0:
+            return False
+    return True
+
 while True:
     print("1. Enter numbers")
     print("2. Exit")
@@ -5,7 +16,15 @@ while True:
 
     if option == "1":
         numbers = input("enter numbers: ")
-        print(numbers)
+        number_list = numbers.split()
+        int_number_list = []
+
+        for number in number_list:
+            int_number_list.append(int(number))
+
+        for number in int_number_list:
+            if is_prime(number) == True:
+                print(number)
     elif option == "2":
         print("bye")
         break
